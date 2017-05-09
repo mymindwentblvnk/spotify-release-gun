7ac275eecb415226a333decb3dfa90bad56f1081
michael-123 <dale.cooper@posteo.de>
2017-05-09 20:20:58 +0200

    Make unittest infrastructure work.

--- tests/test_spotify_release_tweeter.py
+++ tests/test_spotify_release_tweeter.py
@@ -1,9 +1,13 @@
 import unittest
-from mock import test
+from mock import patch
 from hamcrest import assert_that, equal_to, is_
+from spotify_release_tweeter import SpotifyRelease
 
 
-class TestSpotifyReleaseTweeter(unittest.Testcase):
+class TestSpotifyReleaseTweeter(unittest.TestCase):
 
-    def test_test(self):
+    @patch.object(SpotifyRelease, '__init__')
+    def test_test(self, release_init):
+        release_init.return_value = None
+        SpotifyRelease()
         assert_that(4 == 4, is_(True))
